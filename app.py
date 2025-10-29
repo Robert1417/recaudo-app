09b676f7154a53422577bf4e20176988817a6867
Robert1417 <roberto.chapman@gobravo.com.co>
2025-10-29 18:17:56 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Mi primera app", page_icon="💸", layout="centered")
+st.title("💸 Hola, mundo — mi primera app en Streamlit")
+st.write("Si ves este mensaje, ¡ya tienes tu app corriendo gratis!")
